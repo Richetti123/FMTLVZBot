cd61b772267f78c32821735d704ceb22ea9e7a0a
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2023-12-31 18:16:30 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -15,32 +15,6 @@ const fechaActualNum = Date.UTC(
 );
 let q = m.quoted ? m.quoted : m;
 let mime = (q.msg || q).mimetype || q.mediaType || "";
-//INICIO lineas por Kurt18
-const numCelularReenvioImg = "56945086055" //Agregar numero a reenviar Img Ej: 51987000222
-;
-
-console.log(`user.fechaActualBD >${user.fechaActualBD}<`); //Luego comentar
-console.log(`fechaActualNum >${fechaActualNum}<`); //Luego comentar
-console.log(`mime >${mime}<`); //Luego comentar
-
-if (user.fechaActualBD !== fechaActualNum) {  
-    if (/image/g.test(mime)){
-        let img = await q.download?.();        
-        conn.sendFile(
-            `${numCelularReenvioImg}@s.whatsapp.net`,
-            img,
-            "img.jpg",
-            `✅HOLA CREADOR, NUEVO PEDIDO EN CURSO✅\n\nCOMUNIQUESE CON LA PERSONA SU NUMERO ES wa.me/${m.sender.split`@`[0]}`,
-            m,
-            true
-        );
-        m.reply("*⏳GRACIAS POR SU COMPRA, SE ESTA PROCESANDO SU PAGO⏳*\n\n*✅EN BREVES MOMENTOS SE COMUNICARA UNO DE NUESTROS VENDEDORES CONTIGO✅*");
-        user.fechaActualBD = fechaActualNum;  
-        return;
-    }
-}
-//user.fechaActualBD = 0; //test (luego eliminar)
-
 
 let horaActualNum = Date.now();
 const intervaloEsperado = 1 * 60 * 60 * 1000; // 1 hora en milisegundos
